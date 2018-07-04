7be3cc75cce496464a590952f1e8237c05534372
Frank <frankdepaul23@gmail.com>
2018-07-04 18:01:54 -0500

    get/set info

---
+++ AccessModifiers-Get-Set/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1975,9,11));
+            System.Console.WriteLine(person.GerBirthdate());
+
+
+            var person2 = new Person2();
+            person2.Birthdate = new DateTime(1976,3,17);
+            System.Console.WriteLine(person2.Age);
+            
+            
+        }
+    }
+}
