a9ce92aff62191d5ce6eb8b4fec46787b9f30413
Frank <frankdepaul23@gmail.com>
2018-07-07 15:48:33 -0500

    random

---
+++ DojoValidation/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoValidation.Models;
+
+namespace DojoValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create(Ninja ninja)
+        {
+            if(ModelState.IsValid)
+            {
+               return RedirectToAction("Result", ninja);
+                // return View("Success");
+            }
+            else
+            {           
+                return View("Index",ninja);
+                // return RedirectToAction("Process",user);
+            }
+        }
+
+        public IActionResult Result(Ninja ninja)
+        {
+            return View("Result", ninja);
+        }
+
+
+
+
+
+
+
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RandomPasscode/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using RandomPasscode.Models;
+
+namespace RandomPasscode.Controllers
+
+{
+    public class HomeController : Controller
+    {
+        public int counter = 1;
+        
+        
+        public IActionResult Index()
+        {
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var stringChars = new char[14];
+        Random rand = new Random();
+        for(var i = 0; i < stringChars.Length;i++){
+            stringChars[i] = chars[rand.Next(chars.Length)];
+        }
+        var finalString = new String(stringChars);
+        TempData["passcode"] = finalString;
+        // return RedirectToAction("Index");
+
+
+
+
+
+            return View();
+        }
+        public IActionResult Generate()
+        {
+            
+            if(HttpContext.Session.GetInt32("Counter") == null)
+            {
+                HttpContext.Session.SetInt32("Counter", 0);
+            }
+            counter = (int)HttpContext.Session.GetInt32("Counter");
+            counter++;            
+            HttpContext.Session.SetInt32("Counter", counter);                     
+            int? IntVariable = HttpContext.Session.GetInt32("Counter");
+            TempData["TempModel"] = IntVariable; 
+                               
+            return RedirectToAction("Index");
+            
+        }
+
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
